87da1dac20400c639514f405f4887ffcee224255
VitaliyLenin <vitaliylenin77@gmail.com>
2022-11-11 00:20:58 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `);
+
+// instance.show();
+
+const galleryEl = document.querySelector(".gallery");
+
+function makeGallery(item) {
+  const galleryItemsCreate = item
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+        />
+    </a>
+</div>`;
+    })
+    .join("");
+
+  return galleryEl.insertAdjacentHTML("afterbegin", galleryItemsCreate);
+}
