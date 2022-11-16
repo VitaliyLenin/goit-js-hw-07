a4abf261b25ea9b26120273922e58c4f0a9d9c3d
VitaliyLenin <vitaliylenin77@gmail.com>
2022-11-16 12:47:30 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const randerList = (items) =>
+  items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+        />
+    </a>
+</div>`
+    )
+    .join("");
+
+console.log(randerList(galleryItems));
+
+const galleryItemsCreate = (el) => {
+  const galleryEl = document.querySelector(".gallery");
+  galleryEl.insertAdjacentHTML("beforeend", el);
+};
+
+galleryItemsCreate(randerList(galleryItems));
+
+const openGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
